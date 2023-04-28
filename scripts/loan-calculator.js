325e1341ff2420e3aace6413b17c1699414aaafc
Alejandro Regus <Aregus@appdev.yearup.org>
2023-04-28 16:02:22 -0400

    mini-project-base-set

--- scripts/loan-calculator.js
+++ scripts/loan-calculator.js
@@ -1,1 +1,29 @@
-console.log('hello world!');
+`use strict;`;
+
+window.onload = init;
+
+function init() {
+  console.log(`Hello World`);
+  const calculateBtnEl = document.getElementById(`calculate`);
+  calculateBtnEl.onclick = onCalculateBtnClicked;
+}
+
+function onCalculateBtnClicked() {
+  console.log(`clicked`);
+  const loanEl = document.getElementById(`loanAmount`);
+  console.log(loanEl.value);
+  const interestEl = document.getElementById(`interestRate`);
+  console.log(interestEl.value);
+  const termEl = document.getElementById(`loanTerm`);
+  console.log(termEl.value);
+
+  //math tbd
+
+  const monthlyPayment = 500;
+  const totalCost = 500000;
+  const monthlyPaymentEl = document.getElementById(`monthlyPayment`);
+const totalCostEl = document.getElementById(`totalcost`);
+
+  monthlyPaymentEl.innerHTML = monthlyPayment;
+  totalCostEl.innerHTML = totalCost;
+}
